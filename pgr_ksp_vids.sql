5c51f3901cc6cc67f0cdd23ef6cac9a81abf5d8c
geozelot <34125395+geozelot@users.noreply.github.com>
2018-09-05 17:00:43 +0200

    Update pgr_ksp_vids.sql

--- pgr_ksp_vids.sql
+++ pgr_ksp_vids.sql
@@ -1,10 +1,10 @@
--- DROP FUNCTION pgr_ksp(TEXT, BIGINT[], BIGINT, BOOLEAN, BOOLEAN);
+-- DROP FUNCTION pgr_ksp(TEXT, BIGINT[], INTEGER, BOOLEAN, BOOLEAN);
 
 CREATE OR REPLACE FUNCTION pgr_ksp(
 
     IN edges_sql TEXT,
     IN vids BIGINT[],
-    IN k BIGINT,
+    IN k INTEGER,
     IN directed boolean DEFAULT true,
     IN heap_paths boolean DEFAULT false,
     OUT seq integer,
@@ -18,31 +18,39 @@ CREATE OR REPLACE FUNCTION pgr_ksp(
   ) RETURNS SETOF record AS
 
 $BODY$
-
+        
   DECLARE
-
-	len INTEGER := array_length(vids, 1) - 1;
-
-
+  
+  	len INTEGER := array_length(vids, 1) - 1;
+        
   BEGIN
-
-	RETURN QUERY
+  
+    RETURN QUERY
 	
-		SELECT (ROW_NUMBER() OVER())::INTEGER AS seq,
-		       (DENSE_RANK() OVER(PARTITION BY ksp.path_id ORDER BY ksp.path_id))::INTEGER AS path_id,
-		       (ROW_NUMBER() OVER(PARTITION BY ksp.path_id  ORDER BY ksp.path_id))::INTEGER AS path_seq,
-		       ksp.node AS node,
-		       ksp.edge AS edge,,
-		       ksp.COST AS COST,
-		       (ksp.cost + lag(ksp.COST) OVER(PARTITION BY ksp.path_id))::DOUBLE PRECISION AS agg_cost
-		FROM _pgr_ksp(
-				edges_sql::TEXT,
-				vids[n],
-				vids[n+1],
-				cost,
-				reverse_cost
-		     ) AS ksp,
-		     generate_series(1, len) AS n;
+      SELECT (ROW_NUMBER() OVER(ORDER BY ksp.path_id, ksp.itr, ksp.path_seq))::INTEGER AS seq,
+	     (DENSE_RANK() OVER(ORDER BY ksp.path_id))::INTEGER AS path_id,
+	     (ROW_NUMBER() OVER(PARTITION BY ksp.path_id ORDER BY ksp.path_id))::INTEGER AS path_seq,
+	      ksp.node AS node,
+	      ksp.edge AS edge,
+	      ksp.COST AS COST,
+	      CASE
+		WHEN ksp.edge = -1
+		  THEN (SUM(ksp.cost) OVER(PARTITION BY ksp.path_id ORDER BY ksp.itr, ksp.path_seq))::DOUBLE PRECISION
+		  ELSE (SUM(ksp.cost) OVER(PARTITION BY ksp.path_id ORDER BY ksp.itr, ksp.path_seq) - 1)::DOUBLE PRECISION
+	      END AS agg_cost
+      FROM (
+        SELECT (_pgr_ksp(
+                  edges_sql::TEXT,
+                  vids[n],
+                  vids[n+1],
+                  k,
+                  directed,
+                  heap_paths
+              	)).*,
+                n AS itr
+        FROM generate_series(1, len) AS n
+      ) AS ksp
+      WHERE NOT (ksp.edge = -1 AND itr < len);
 
   END
 
@@ -50,4 +58,4 @@ $BODY$
 
 LANGUAGE plpgsql VOLATILE
 COST 100
-ROWS 1000	
+ROWS 1000
